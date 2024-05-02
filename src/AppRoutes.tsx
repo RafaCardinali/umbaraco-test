2d5b9d9a2e5ba5302867b1012a3014eea5bd4dbf
Rafael Cardinali <rflcardinali@gmail.com>
2024-05-01 21:21:22 -0300

    feat(home): home creation

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ProfessionalRegister from './components/professional-register/ProfessionalRegister';
+import Home from './components/home/Home';
 
 const AppRoutes = () => {
   return (
     <Routes>
+        <Route path='/' element={<Home />}/>
         <Route path="/professional-register" element={<ProfessionalRegister />} />
+        <Route path='*' element={<Home />}/>
     </Routes>
   );
 };
